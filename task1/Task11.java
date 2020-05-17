d3fcbd144ec154375475ca3ee77b3cb096231f58
KlishchRoman <klisch.roma@gmail.com>
2020-05-17 18:47:07 +0300

    task2

--- task1/Task11.java
+++ task1/Task11.java
@@ -6,6 +6,11 @@ public static void main(String... args){
 char[] arr = {'a','b','c'};
 showArrays(arr);
 }
+
+/*
+task 1
+*/
+
 /*
 public static void showArrays(char[] arr){
 for(char s:arr){
@@ -13,8 +18,19 @@ System.out.print(s+", ");
 }
 }
 */
-public static void showArrays(char[] arr){
+public static void showArrays(char[] charArr){
 System.out.print(Arrays.toString(arr));
 }
+
+/*
+task 2
+*/
+
+public static char[] intToChar(int[] intArr){
+char[] charArr =Arrays.stream(intArr).map(x->(char)x).toArray(); 
+return charArr;
+}
+
+
 
 }
